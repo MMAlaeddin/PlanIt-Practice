e29141b3d97dc54de40a0f24f1d90edd80e6afd1
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-08 10:12:21 -0700

    start on plan cs file to start building initial busplan constructor

---
+++ PlanIt/Models/Plan.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PlanIt.Models
+{
+  public class businessPlan
+  {
+    public int PlanID { get; set; }
+    public string BusName { get; set; }
+
+  }
+}
